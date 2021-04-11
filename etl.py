d94dedc6953d5b2d22d11483d47d76612afc6cc3
Aravind Raja <melarcodearavind@gmail.com>
2021-04-11 14:23:06 +0000

    Changed Songplays datatype to SERIES

--- etl.py
+++ etl.py
@@ -6,6 +6,16 @@ from sql_queries import *
 
 
 def process_song_file(cur, filepath):
+    '''
+    Load Song Json file as Dataframe and Inserts data into Tables Songs and Artist.
+
+            Parameters:
+                    cur : Cursor for Database
+                    filepath: Path of the Source file
+
+            Returns:
+                    None
+    '''
     # open song file
     df = pd.read_json(filepath, lines=True) 
 
@@ -21,11 +31,21 @@ def process_song_file(cur, filepath):
 
 
 def process_log_file(cur, filepath):
+        '''
+    Load Log Json file as Dataframe and Inserts data into Tables Time , Users and Songplays.
+
+            Parameters:
+                    cur : Cursor for Database
+                    filepath: Path of the Source file
+
+            Returns:
+                    None
+    '''
     # open log file
     df = pd.read_json(filepath, lines=True) 
 
     # filter by NextSong action
-    df = df[df['page']=='NextSong']
+    time_data = df.loc[df['page'] == 'NextSong']
 
     # convert timestamp column to datetime
     t =pd.to_datetime(df.ts, unit='ms') 
@@ -44,6 +64,8 @@ def process_log_file(cur, filepath):
 
     # insert user records
     for i, row in user_df.iterrows():
+        if row.userId == ''  or not row.userId:
+            continue
         cur.execute(user_table_insert, row)
 
     # insert songplay records
@@ -61,11 +83,23 @@ def process_log_file(cur, filepath):
         start_time = pd.to_datetime(row.ts)
         
         # insert songplay record
-        songplay_data = (index,start_time,row.userId,row.level,songid,artistid,row.sessionId,row.location,row.userAgent)
+        songplay_data = (start_time,row.userId,row.level,songid,artistid,row.sessionId,row.location,row.userAgent)
         cur.execute(songplay_table_insert, songplay_data)
 
 
 def process_data(cur, conn, filepath, func):
+        '''
+   It retrives all the json files and Inserts all data using Function Call.
+
+            Parameters:
+                    cur     : Cursor for Database
+                    conn    : Connection to the Database
+                    filepath: Path of the Source file
+                    func    : Function to be invoked
+
+            Returns:
+                    None
+    '''
     # get all files matching extension from directory
     all_files = []
     for root, dirs, files in os.walk(filepath):
@@ -85,6 +119,14 @@ def process_data(cur, conn, filepath, func):
 
 
 def main():
+    '''
+   The Main function etablishes connection to database and invokes other functions in order of execution .
+
+            Parameters:
+                    None
+            Returns:
+                    None
+    '''
     conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=student password=student")
     cur = conn.cursor()
 
--- sql_queries.py
+++ sql_queries.py
@@ -8,43 +8,43 @@ time_table_drop = "drop table IF EXISTS time;"
 
 # CREATE TABLES
 
-songplay_table_create = ("""Create table songplays (songplay_id integer PRIMARY KEY ,start_time timestamp,user_id varchar,level varchar,song_id varchar ,artist_id varchar,session_id int,location varchar,user_agent varchar)  ;
+songplay_table_create = ("""Create table songplays (songplay_id SERIAL PRIMARY KEY NOT NULL ,start_time timestamp,user_id varchar,level varchar,song_id varchar ,artist_id varchar,session_id int,location varchar,user_agent varchar)  ;
 """)
 
-user_table_create = ("""Create table users (user_id integer,first_name varchar,last_name varchar,gender varchar,level varchar);
+user_table_create = ("""Create table users (user_id int PRIMARY KEY NOT NULL,first_name varchar,last_name varchar,gender varchar,level varchar);
 """)
 
-song_table_create = ("""Create table songs (song_id varchar, title varchar,artist_id varchar, year integer,duration decimal);
+song_table_create = ("""Create table songs (song_id varchar PRIMARY KEY NOT NULL, title varchar,artist_id varchar, year integer,duration decimal);
 """)
 
-artist_table_create = ("""Create table artists (artist_id varchar,name varchar,location varchar,latitude decimal,longitude decimal);
+artist_table_create = ("""Create table artists (artist_id varchar PRIMARY KEY NOT NULL,name varchar,location varchar,latitude decimal,longitude decimal);
 """)
 
-time_table_create = ("""Create table time(start_time timestamp,hour integer,day integer,week integer,month integer,year integer,weekday varchar);
+time_table_create = ("""Create table time(start_time timestamp PRIMARY KEY NOT NULL,hour integer,day integer,week integer,month integer,year integer,weekday varchar);
 """)
 
 # INSERT RECORDS %s,
 
-songplay_table_insert = ("""Insert into songplays (songplay_id,start_time ,user_id,level,song_id,artist_id ,session_id ,location ,user_agent) Values (%s,%s,%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (songplay_id) DO NOTHING;
+songplay_table_insert = ("""Insert into songplays (start_time ,user_id,level,song_id,artist_id ,session_id ,location ,user_agent) Values (%s,%s,%s,%s,%s,%s,%s,%s);
 """)
 
-user_table_insert = ("""Insert into users (user_id ,first_name ,last_name ,gender ,level ) Values (%s,%s,%s,%s,%s);
+user_table_insert = ("""Insert into users (user_id ,first_name ,last_name ,gender ,level ) Values (%s,%s,%s,%s,%s) ON CONFLICT (user_id) DO UPDATE SET level=EXCLUDED.level;
 """)
 
-song_table_insert = ("""Insert into songs (song_id,title,artist_id,year,duration) Values (%s,%s,%s,%s,%s);
+song_table_insert = ("""Insert into songs (song_id,title,artist_id,year,duration) Values (%s,%s,%s,%s,%s) ON CONFLICT(song_id) DO NOTHING;
 """)
 
-artist_table_insert = ("""Insert into artists (artist_id ,name ,location ,latitude ,longitude) Values (%s,%s,%s,%s,%s);
+artist_table_insert = ("""Insert into artists (artist_id ,name ,location ,latitude ,longitude) Values (%s,%s,%s,%s,%s) ON CONFLICT(artist_id) DO NOTHING;
 """)
 
 
-time_table_insert = ("""Insert into time (start_time ,hour ,day ,week ,month ,year ,weekday ) Values (%s,%s,%s,%s,%s,%s,%s);
+time_table_insert = ("""Insert into time (start_time ,hour ,day ,week ,month ,year ,weekday ) Values (%s,%s,%s,%s,%s,%s,%s) ON CONFLICT (start_time) DO NOTHING;
 """)
 
 # FIND SONGS
 
 song_select = ("""
-SELECT songs.song_id,artists.artist_id FROM  songs INNER JOIN artists ON songs.artist_id = artists.artist_id WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s;
+SELECT songs.song_id,songs.artist_id FROM  songs INNER JOIN artists ON songs.artist_id = artists.artist_id WHERE songs.title = %s AND artists.name = %s AND songs.duration = %s;
 """)
 
 # QUERY LISTS
